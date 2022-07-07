da9fa454327e1cd3715954d694bbc4e1f172c420
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-07-07 23:30:30 +0100

    update db url

--- src/database/db_client.ts
+++ src/database/db_client.ts
@@ -2,7 +2,7 @@ import config from '../config';
 import mongoose from 'mongoose';
 import logger from '../utilities/logger';
 
-const MONGO_URL = `mongodb+srv://${config.APP_DB_USERNAME}:${config.APP_DB_PASSWORD}@cluster0.epdkq.mongodb.net/${config.APP_DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${config.APP_DB_USERNAME}:${config.APP_DB_PASSWORD}@cluster0.q9g0p.mongodb.net/${config.APP_DB_NAME}?retryWrites=true&w=majority`;
 
 const connectToDbClient = async (uri: string = MONGO_URL): Promise<void> => {
   try {
